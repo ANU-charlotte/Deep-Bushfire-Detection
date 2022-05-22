6675220064a5c386cf16b1862537e65432fb64c1
unknown <charlotte.kwong@anu.edu.au>
2022-05-22 20:00:07 +1000

    Changed from torch device cuda to CPU because cuda wasn't running on my pc

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ RESIZE_TO = 416
 NUM_EPOCHS = 10
 NUM_WORKERS = 4
 
-DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-
+#DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+DEVICE = torch.device('cpu')
 # Training and Validation directory
 TRAIN_DIR = 'Deep Smoke Detection.v1i.voc (1)/train'
 VALID_DIR = 'Deep Smoke Detection.v1i.voc (1)/valid'
